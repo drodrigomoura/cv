a8589a04aaf1fbcb7d1076cdfc106e6d658248c2
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-04-12 23:21:42 -0300

    programando el contenido de los componentes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Experience = () => (
     <div className="Experience">
         <div className="Experience-container">
-            <dic className="Experience-item">
+            <div className="Experience-item">
                 <h3>Experiencia Laboral</h3>
                 <p>Backend Developer - Escuela de Robotica, Misiones</p>
-            </dic>
+            </div>
         </div>
     </div>
 )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 const Main = ({ children }) => (
     <div className="Main ">
         <div className="Main-container">
-            <p>sdasdas</p>
             {children}
         </div>
     </div>
