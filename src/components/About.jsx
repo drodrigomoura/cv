82c38856703c82996ea6a004452989c7f42a6da2
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-04-14 12:23:30 -0300

    Pasando datos a skills y social

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Social from './Social'
 
 const About = ({ avatar, name, profession, bio, address, social }) => (
     <div className="About">
@@ -23,7 +24,7 @@ const About = ({ avatar, name, profession, bio, address, social }) => (
                 <p>{address}</p>
             </div>
             <div className="About-social">
-                {/* {social} */}
+                <Social social={social} />
             </div>
         </div>
     </div>
