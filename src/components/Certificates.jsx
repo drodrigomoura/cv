2e74b5295d6e6c563cc52514f68b10ddee2a2c51
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-04-16 22:12:22 -0300

    Agregando Custom Styles

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import H2Styled from '../styled/H2Styled'
+import H3Styled from '../styled/H3Styled'
+import PStyled from '../styled/PStyled'
 
 const Certificates = props => (
     <div className="Certificates">
+        <H2Styled name="Certificates" />
         <div className="Certificates-container">
             {
                 props.data.map((cert, index) => (
                     <div className="Certificates-item" key={`Cert-${index}`}>
-                        <h3>{cert.name} @ {cert.institucion}
+                        <H3Styled>{cert.name} @ {cert.institucion}
                             <span>{cert.date}</span>
-                        </h3>
-                        <p>{cert.description}</p>
+                        </H3Styled>
+                        <PStyled>{cert.description}</PStyled>
                     </div>
                 ))
             }
