c255360caabdbe90053521aedab6850491986c88
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-07-25 11:41:34 -0300

    Animaciones con Styled Components

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 const SkillsContainer = styled.div`
     display: grid;
@@ -9,14 +9,19 @@ const SkillsContainer = styled.div`
 `;
 
 const SkillsH5 = styled.h5`
-    margin: .5em 0;     
+    margin: 0.5em 0;     
 `;
 
 const SkillLine = styled.div`
-    heigh: 8px;
+    height: 8px;
     position: relative;
     border-radius: 50px;
-    background: #f8bbd0;
+    background: #84f5c2;
+`;
+
+const move = keyframes`
+    0% { background-position: 0 0;}
+    100% { background-position: 50px 50px; }
 `;
 
 const SkillsSpan = styled.span`
@@ -28,6 +33,19 @@ const SkillsSpan = styled.span`
     overflow: hidden;
     position: relative;
     box-shadow: inset 0 2px 9px rgba(255,255,255,0.3), inset 0 -2px 6px rgba(0,0,0,0.4);
+    &:after{
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        background-image: linear-gradient(-45deg, rgba(255,255,255,0.2) 25%, transparent 25%, transparent 50%, rgba(255,255,255,0.2) 50%, rgba(255,255,255,0.2) 75%, transparent 75%, transparent);
+        z-index: 1;
+        background-size: 50px 50px;
+        animation: ${move} 4s linear infinite;
+        overflow: hidden;
+    }
 `;
 
 const Skills = props => (
@@ -47,4 +65,4 @@ const Skills = props => (
     </div>
 )
 
-export default Skills
+export default Skills;
